caa209701f6e97c548ac9d5fa7217a9b8fb025ff
Andrei Malosti <andrei.malosti06@gmail.com>
2024-02-23 16:12:12 -0300

    endpoint to return posts of a user

--- src/main/java/com/andrei/workshopmongo/resources/UserResources.java
+++ src/main/java/com/andrei/workshopmongo/resources/UserResources.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.andrei.workshopmongo.domain.Post;
 import com.andrei.workshopmongo.domain.User;
 import com.andrei.workshopmongo.dto.UserDTO;
 import com.andrei.workshopmongo.services.UserService;
@@ -55,5 +56,10 @@ public class UserResources {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+}
+	@RequestMapping(value="/{id}/posts" ,method = RequestMethod.GET)
+	public ResponseEntity <List<Post>> findPosts (@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 }
 	}
